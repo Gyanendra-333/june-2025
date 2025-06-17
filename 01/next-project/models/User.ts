49487c73733a04794fabeb7c30e643c64fb352fe
Gyanendra <pgyanendra333@gmail.com>
2025-06-17 18:47:36 +0530

    model

--- 01/next-project/models/User.ts
+++ 01/next-project/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { model, models, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 
 
@@ -26,3 +26,13 @@ const userSchema = new Schema<IUser>(
 );
 
 
+userSchema.pre('save', async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+const User = models?.User || model<IUser>("User", userSchema);
+
+export default User;
--- 01/next-project/models/Video.ts
+++ 01/next-project/models/Video.ts
@@ -1,1 +1,14 @@
- 
+import mongoose, { model, models, mongo, Schema } from "mongoose";
+
+export const VIDEO_DIMENSIONS = {
+    width: 1080,
+    height: 1920
+} as const;
+
+export interface IVideo {
+    _id?: mongoose.Types.ObjectId;
+    title: string;
+    description: string;
+    videoURL: string;
+    thumbnailURL: string;
+}
